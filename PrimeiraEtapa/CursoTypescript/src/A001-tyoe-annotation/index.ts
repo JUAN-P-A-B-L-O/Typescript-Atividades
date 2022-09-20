9fced589c378365fa3d6903290956aa8bdec4c8f
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2022-09-19 22:22:07 -0300

    Atualizando arquivos prontos

---
+++ PrimeiraEtapa/CursoTypescript/src/A001-tyoe-annotation/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+let nome: string = `Juan Pablo`;
+
+
+let numberArray: number[] =[];
+numberArray.push(2);
+numberArray.push(4);
+numberArray.push(2+22);
+console.log(numberArray);
+
+let pessoa: {nome: string,idade: number,adulto?: boolean}={
+    nome: `Juan`,
+    idade: 20,
+}
+
+console.log(pessoa.idade)
+
+function soma(x:number, y: number): number{
+    return x+ y;
+}
+
+const resultado = soma(5,6);
+console.log(resultado);
+
+const soma2: (x:number, y:number) =>number = (x,y) => x+y;
--- PrimeiraEtapa/CursoTypescript/src/index.ts
+++
@@ -1,2 +0,0 @@
-const nome = `Juan`;
-console.log(nome);
